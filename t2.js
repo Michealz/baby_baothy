0da0ecbe6462fd5834fe5f4efc9b3dc1ee84aca0
Dương Tuấn Anh <anhdt153@vinhomes.vn>
2024-04-16 14:53:02 +0700

    upload

--- t2.js
+++ t2.js
@@ -1,30 +1,45 @@
-function fadeIn(){
-    var text = $('.content').text().replace(/^\s+|\s+$/g,'');
+function fadeIn() {
+    var text = $('.content').text().replace(/^\s+|\s+$/g, '');
     var length = text.length;
     var i = 0;
     var txt;
     var html = [];
     var sp = 4;
-    for(;i<length;i+=sp) {
-        txt = text.substring(i,i+sp);
+    for (; i < length; i += sp) {
+        txt = text.substring(i, i + sp);
         html.push('<span class="c animated">' + txt + '</span>');
     }
     $('.content').removeClass('c').html(html.join(''));
 
-    for(i = 0,length = html.length;i<length;i++) {
-        !function(i){
-            setTimeout(function(){
+    for (i = 0, length = html.length; i < length; i++) {
+        ! function(i) {
+            setTimeout(function() {
                 $('.content').find('.animated').eq(i).addClass('fadeIn');
-            },i*400);
+            }, i * 400);
         }(i);
     }
 };
 
+$('#audioplay').click(() => {
+    let audio = $('#audio')[0];
+    audio.play()
+})
+
+$('#box-body').click(() => {
+    setTimeout(() => {
+        let audio = $('#audio')[0];
+        audio.play()
+        document.querySelector("#main").hidden = false
+        document.querySelector("#container").hidden = true
+        document.querySelector("#heart").hidden = false
+        document.querySelector("body").style.backgroundColor = "#542246"
+        document.querySelector("#heart").hidden = false
+        fadeIn()
+    }, 1200)
+})
 
 
-window.onload = () =>{
-    document.querySelector("#heart").hidden=false
-    document.querySelector("body").style.backgroundColor="#542246"
-    document.querySelector("#heart").hidden=false
-    fadeIn()
+
+window.onload = () => {
+
 }
